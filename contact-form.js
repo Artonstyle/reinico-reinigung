9e403fa1db56de796367db037b8711b749470751
Arton <artonstyle@hotmail.fr>
2026-09-14 00:17:45 +0200

    Update deployment for contact notification emails

--- contact-form.js
+++ contact-form.js
@@ -1,6 +1,6 @@
 (() => {
     // Vorherige URL für Rückwechsel: https://script.google.com/macros/s/AKfycby2gR3eRqy2Hjs8sMQTrxHIAC_qh-nWoMKlagt7rkYjWUVyBh30MvcGnldLY9mKHcg/exec
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxgsAsq3dh6dcNoCvfG6eaaP5EKg-n6K0TZ1HOhikAf0YudaVHoYsipwe7GkbL_SZA/exec';
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzosKzOhOzh77oYo4_1HCH-ANff7XLbxQlVLfbQpS71DGeAbSwUiLlEQavnPDoBSd8/exec';
 
     document.querySelectorAll('form[data-reinico-contact]').forEach(form => {
         const button = form.querySelector('button[type="submit"]');
